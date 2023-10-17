86c63c17ce0748c24387deb408f9292cd1474097
stiva98 <samustiva9810@gmail.com>
2023-10-17 16:15:24 +0200

    stilizzato header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -9,39 +9,37 @@ export default {
 
 <template>
   <div>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg bg-body-white">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand p-0" href="#">
+      <div class="d-flex align-items-center">
+        <img src="src/assets/img/logo-header.svg" alt="" class="img-header">
+        <h2 class="px-3">
+          Bool BNB
+        </h2>
+      </div>
+    </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link link-hover" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
+          <a class="nav-link link-hover" href="#">Ricerca</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          <a class="nav-link link-hover" href="#">Dashboard</a>
         </li>
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+      <a href="#" class="btn btn-outline-warning">
+        Accedi
+      </a>
+      <a href="#" class="btn btn-outline-success ms-3">
+        Registrati
+      </a>
     </div>
   </div>
 </nav>
@@ -51,5 +49,15 @@ export default {
 </template>
 
 <style lang="scss" >
-
+.img-header {
+  max-width: 70px;
+}
+.link-hover:hover {
+  text-decoration: underline;
+}
+.nav-item:hover {
+  a {
+    color: orangered;
+  }
+}
 </style>
