5c482fc50899357120cb25c74becf82982238c51
Simo-bsc <simonebuscemi00@gmail.com>
2023-10-18 23:37:14 +0200

    Card e BG/Homepage Modificati e Funzionanti in MobileVersion

--- src/components/Main/CardComponent.vue
+++ src/components/Main/CardComponent.vue
@@ -1,10 +1,6 @@
 <script>
-import AccordCardComponent from './AccordCardComponent.vue';
 
 export default {
-        components: {
-        AccordCardComponent,
-    },
     data() {
     	return 
     },
@@ -12,41 +8,69 @@ export default {
 </script>
 
 <template>
-        <div class="card mb-3" style="max-width: 540px;">
-                <div class="row g-0">
-                        <div class="col-md-4">
-                                <img src="/src/assets/img/cover1.jpg" class="img-fluid rounded-start shadoww" alt="...">  
-                                <a class="btn btn-warning mt-3 ms-2" href="#">Apri</a>
-                        </div>
-
-                        <div class="col-md-8">
-                                <div class="card-body position-relative">
-                                        <h4 class="card-title d-inline-block">Casa in montagna</h4> 
-                                        <p class="card-text">Via Roma, 34, Roma</p>
-                                        <p class="position-price p-1">300€ /N</p>
-                                        
-
-                                        <AccordCardComponent />
-                                </div>
-                        </div>
-                </div>
+    <div class="position-relative mobile_size">
+        <!-- IMG -->
+        <a href="#">
+            <img src="/src/assets/img/cover1.jpg" class="img-card rounded d-block shadoww" alt="...">
+        </a>
+        <!-- CONTAINER INFO -->
+        <div class="container_ position_container p-2 rounded shadoww">
+            <p>Via Roma, 34, Roma</p>
+            <p class="position_price style_price p-1">300€ /N</p>
+            <p><strong>Titolo appartamento</strong></p>
+            <hr>
+            <div class="info">
+                <p><i class="fa-solid fa-door-open"></i> Stanze: 2 <span class="ps-2"><i class="fa-solid fa-maximize"></i> M²: 50</span></p> 
+                <p><i class="fa-solid fa-bed"></i> Letti: 4 <span class="ps-3"><i class="fa-solid fa-bath"></i> Bagni: 1</span></p>
+            </div>
         </div>
+    </div>                                    
 </template>
 
 <style lang="scss" scoped>
 
-    .shadoww{
-    box-shadow: 5px 5px 5px 3px rgb(0, 0, 0, .5);
+     .shadoww{
+     box-shadow: 0px 5px 5px 3px rgb(0, 0, 0, .5);
+     }
+
+    .img-card{
+        height: 350px;
+        width: 100%;
     }
 
-    .position-price{
-        position: absolute;
-        right: 20px;
-        top: 65px;
+    .container_{
+        width: 80%;
+        background-color: white;
+    }
 
-        border: 2px solid #FFCA2C;
-        border-radius: 5px;
+    .position_container{
+        position: relative;
+        bottom: 50px;
+        left: 25px;
+        
+    }
+
+    .position_price{
+        position: absolute;
+        right: 0px;
+        top: -15px;
+    }
+
+    .style_price{
         background-color: #FFCA2C;
+        border: 1px solid #FFCA2C;
+        border-top-right-radius: 10px;
+        border-bottom-left-radius: 10px;
+    }
+
+    .info{
+        p{
+		    font-weight: bold;
+
+            i{
+                color: orange;
+            }
+	    }
     }
 
 </style>
